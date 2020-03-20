eeeb8a38aad3cebf6104a1c811b46670ce58f200
icevisual <icevisual@hotmail.com>
2020-03-20 18:37:39 +0800

    u

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -3,8 +3,10 @@
 
 #include "pch.h"
 #include <sstream>
+#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 #include <map>
 using namespace std;
 
@@ -22,26 +24,168 @@ string vectorInt2String(vector<int> arr) {
 	return str;
 }
 
-void printVector(vector<vector<int>> arr) {
-	if (arr.size() == 0) {
-		cout << "[]" << endl;
-		return;
-	}
-	cout << "[" << endl;
-	for (int i = 0; i < arr.size(); i++) {
-		cout << "  " << vectorInt2String(arr[i]) << "," << endl;
-	}
-	cout << "]" << endl;
+void printVectorInt(vector<int> arr) {
+	std::cout << vectorInt2String(arr) << endl;
 }
 
 
+void printVectorVectorInt(vector<vector<int>> arr) {
+	if (arr.size() == 0) {
+		std::cout << "[]" << endl;
+		return;
+	}
+	std::cout << "[" << endl;
+	for (int i = 0; i < arr.size(); i++) {
+		std::cout << "  " << vectorInt2String(arr[i]) << "," << endl;
+	}
+	std::cout << "]" << endl;
+}
+
 class Solution {
 public:
+
+	vector<vector<int>> fourSum(vector<int>& nums, int target) {
+		vector<vector<int>> result;
+		unordered_map<string, bool>  ext;
+		unordered_map<int, vector<vector<int>>> hash;
+		unordered_map<int, vector<vector<int>>>::iterator l_it;
+
+		sort(nums.begin(),nums.end());
+
+		for (int i = 0; i < nums.size(); i++) {
+
+			for (int j = i + 1; j < nums.size(); j++) {
+
+				int s = nums[i] + nums[j];
+
+				l_it = hash.find(target - s);
+				if (l_it != hash.end())
+				{
+					vector<int> pr = { l_it->second[0][0],l_it->second[0][1] , nums[i], nums[j] };
+					result.push_back(pr);
+			/*		sort(pr.begin(), pr.end());
+
+					ostringstream ostrStream;
+					ostrStream << pr[0] << ",";
+					ostrStream << pr[1] << ",";
+					ostrStream << pr[2] << ",";
+					ostrStream << pr[3];
+					 
+					if (ext.find(ostrStream.str()) == ext.end()) {
+						result.push_back(pr);
+						ext.insert(pair<string,bool>(ostrStream.str(),true));
+					}*/
+				}
+				else {
+
+					l_it = hash.find(s);
+					if (l_it != hash.end()) {
+						hash[s].push_back({nums[i], nums[j]});
+					}
+					else
+					{
+						hash.insert(pair<int, vector<vector<int>>>(s, { {nums[i], nums[j]} }));
+					}
+				}
+			}
+		}
+
+
+		return result;
+	}
+	vector<int> twoSum(vector<int>& nums, int target) {
+		unordered_map<int, int> _map;
+		vector<int> rrr;
+		unordered_map<int, int>::iterator l_it;
+		for (int i = 0; i < nums.size(); i++)
+		{
+			int sub = target - nums[i];
+
+			l_it = _map.find(sub);
+			if (l_it != _map.end()
+				&& l_it->second != i)
+			{
+				rrr.push_back(i);
+				rrr.push_back(l_it->second);
+				return rrr;
+			}
+			_map.insert(pair<int, int>(nums[i], i));
+		}
+		return rrr;
+	}
+
+
+	int threeSumClosest(vector<int>& nums, int target) {
+
+		sort(nums.begin(), nums.end());
+		vector<vector<int>> result;
+		int length = nums.size();
+
+		int min = 999999;
+		int min_sum = 0;
+
+		for (int i = 0; i < length; i++) {
+			int j = i + 1;
+			int k = length - 1;
+	
+			if (i > 0 && nums[i] == nums[i - 1])
+				continue;
+			while (j < k) {
+				int s = nums[i] + nums[j] + nums[k];
+
+				if (abs(s - target) < min) {
+					min = abs(s - target);
+					min_sum = s;
+				}
+
+				if (s == target) {
+					return target;
+				}
+				else if (s > target) {
+					k--;
+				}
+				else {
+					j++;
+				}
+			}
+
+		}
+
+		return min_sum;
+	}
+
 	vector<vector<int>> threeSum(vector<int>& nums) {
 
 		vector<vector<int>> result;
 
+		sort(nums.begin(), nums.end());
 
+		int length = nums.size();
+		for (int i = 0; i < length; i++) {
+			int j = i + 1;
+			int k = length - 1;
+			if (nums[i] > 0)
+				break;
+			if (i > 0 && nums[i] == nums[i - 1]) 
+				continue;
+			while (j < k) {
+				int s = nums[i] + nums[j] + nums[k];
+				if (s == 0) {
+					result.push_back({ nums[i], nums[j] , nums[k] });
+					while (j < k && nums[j + 1] == nums[j]) j++;
+					while (j < k && nums[k - 1] == nums[k]) k--;
+					j++;
+					k--;
+				}
+				else if (s > 0) {
+					k--;
+				}
+				else {
+					j++;
+				}
+			}
+
+		}
 		return result;
 	}
 };
@@ -49,9 +193,14 @@ public:
 
 int main()
 {
-	vector<int> v = { -1, 0, 1, 2, -1, -4 };
+	vector<int> v = { 1, 0, -1, 0, -2, 2 };
+	vector<int> v1 = { 1, 0, -1, 0, -2, 2 };
+	vector<int> v2 = {1,1,1,1 };
+	
 	Solution so;
-	printVector(so.threeSum(v));
+	printVectorVectorInt(so.fourSum(v,0));
+	printVectorVectorInt(so.fourSum(v1, 0));
+
 	system("pause");
 	return 0;
 }
