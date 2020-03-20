27eff87f9ffb6872066590c6afe67174711e1a7d
icevisual <icevisual@hotmail.com>
2020-03-20 15:04:02 +0800

    Add Print func

--- Solution/Solution.cpp
+++ Solution/Solution.cpp
@@ -2,42 +2,56 @@
 //
 
 #include "pch.h"
+#include <sstream>
 #include <iostream>
 #include <vector>
 #include <map>
 using namespace std;
 
+string vectorInt2String(vector<int> arr) {
+
+	string str = "[";
+
+	for (int i = 0; i < arr.size(); i++) {
+		ostringstream ostrStream;
+		ostrStream << arr[i];
+		str.append(ostrStream.str());
+		str.append(", ");
+	}
+	str.append("]");
+	return str;
+}
+
+void printVector(vector<vector<int>> arr) {
+	if (arr.size() == 0) {
+		cout << "[]" << endl;
+		return;
+	}
+	cout << "[" << endl;
+	for (int i = 0; i < arr.size(); i++) {
+		cout << "  " << vectorInt2String(arr[i]) << "," << endl;
+	}
+	cout << "]" << endl;
+}
+
+
 class Solution {
 public:
 	vector<vector<int>> threeSum(vector<int>& nums) {
 
+		vector<vector<int>> result;
+
+
+		return result;
 	}
 };
 
 
-
-
-
 int main()
 {
-
-
-
-
-
-
-
+	vector<int> v = { -1, 0, 1, 2, -1, -4 };
+	Solution so;
+	printVector(so.threeSum(v));
 	system("pause");
 	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
